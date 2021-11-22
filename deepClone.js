f94016a4e981e45854f52d6da80ccf2185fff5c2
King1376 <1185079648@qq.com>
2021-11-23 00:06:30 +0800

    feat: 实现深克隆

--- deepClone.js
+++ deepClone.js
@@ -1,67 +1,59 @@
-/**
- * 深克隆
- * @param {克隆对象} target 
- * @param {缓存} cache 
- */
-const deepClone = (target, cache = []) => {
-    // 数据类型
-    const type = isType(target)
-    // 解决自应用问题，遍历缓存，若存在，则直接返回
-    for(let item of cache) {
-        const { tar, res } = item
-        if(tar === target) {
-            return res
-        }
-    }
+const getType = target => Object.prototype.toString.call(target).slice(8, -1)
 
-    // 最终结果
-    let result = dataTypeBycreate(type);
+const isBaseType = [
+    'Number',
+    'String',
+    'Boolean',
+    'Null',
+    'Undefined',
+    'Symbol'
+]
 
-    cache.push({ tar: target, res: result })
-    if(isBaseTypes.includes(type)) {        // 基本数据类型
-        result = target
-    }else if(type === 'Date') {             // 日期类型   
-        result = new Date(target)
-    }else if(type === 'RegExp') {           // 正则表达式
-        result = new RegExp(target.source, target.flags)        
-    }else if(type === 'Function') {         // 函数
-        result = target.bind(this)
-    }else if(type === 'Set') {              // Set        
-        for(let item of target) {
-            result.add(deepClone(item,cache))
-        }
-    }else if(type === 'Map') {              // Map
-        for(let item of target) {
-            const [key, value] = item
-            result.set(key,deepClone(value,cache))
-        }
-    }else if(['Array', 'Object'].includes(type)) {      // 数组和对象
-        for(let key in target) {
-            result[key] = deepClone(target[key],cache)
-        }
-    }
-    return result
-}
-
-// 根据数据类型相应的初始化
-const dataTypeBycreate = type => {
-    const createMaps = {
+const creatRes = type => {
+    const maps = {
         'Array': [],
         'Object': {},
         'Set': new Set(),
         'Map': new Map(),
     }
-    return createMaps[type]
+    return maps[type]
 }
 
-// 数据类型
-const isType = data => {
-    return Object.prototype.toString.call(data).slice(8, -1)
+const deepClone = (target, cache = []) => {
+    for(let item of cache) {
+        const {tar, res} = item ?? {}
+        if(target === tar) {
+            return res
+        }
+    }
+
+    const type = getType(target)
+    let result = creatRes(type)
+    cache.push({tar: target, res: result})
+    if(isBaseType.includes(type)) {
+        result = target
+    } else if(type === 'Date') {
+        result = new Date(target)
+    } else if(type === 'RegExp') {
+        result = new RegExp(target.source, target.flags)
+    } else if(type === 'Function') {
+        result = target.bind(this)
+    } else if(['Array', 'Object'].includes(type)) {
+        for(let key in target) {
+            result[key] = deepClone(target[key], cache)
+        }
+    } else if(type === 'Set') {
+        for(let item of target) {
+            result.add(deepClone(item, cache))
+        }
+    } else if(type === 'Map') {
+        for(let item of target) {
+            const [key, value] = item
+            result.set(key, deepClone(value, cache))
+        }
+    }
+    return result
 }
-
-// 基本数据类型
-const isBaseTypes = [ 'Number', 'String', 'Boolean', 'Null', 'Undefined', 'Symbol']
-
 
 // 测试
 const testObj = {
@@ -94,4 +86,4 @@ newData.ma.set('copyKey',Symbol('copy_map')),
 newData.time = new Date('2021-09-10'),
 newData.regExp = /\.js/g
 
-// console.log(testObj,newData, testObj == newData)
+console.log(testObj,newData, testObj == newData)
